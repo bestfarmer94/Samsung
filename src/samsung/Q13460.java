4d8bada35c8924c497c15e8c1ada4d1d80590e99
bestfarmer94 <sjasjaruddus@naver.com>
2022-10-14 19:45:44 +0900

    Samsung7

--- src/samsung/Q13460.java
+++ src/samsung/Q13460.java
@@ -3,18 +3,14 @@ package samsung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q13460 {
-
-	static int result;
-	static int[] dx;
-	static int[] dy;
-	static PriorityQueue<RBO> pq;
 	
 	public static void main(String[] args) throws IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,149 +18,133 @@ public class Q13460 {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		String[][] map = new String[n+1][m+1];
-		int x = 0;
-		int y = 0;
-		int x_b = 0;
-		int y_b = 0;
+		String[][] map = new String[n][m];
 		
-		for(int i=1; i<n+1; i++) {
-			String[] str = br.readLine().split("");
+		Queue<Ball> q = new LinkedList<Ball>();
+		int x = 0; int y = 0;
+		int x2 = 0; int y2 = 0;
+		
+		for(int i=0; i<n; i++) {
+			String str = br.readLine();
 			
-			for(int j=1; j<m+1; j++) {
-				map[i][j] = str[j-1];
+			for(int j=0; j<m; j++) {
+				map[i][j] = Character.toString(str.charAt(j));
+				
 				if(map[i][j].equals("R")) {
 					x = i;
 					y = j;
 				}
 				
 				if(map[i][j].equals("B")) {
-					x_b = i;
-					y_b = j;
+					x2 = i;
+					y2 = j;
 				}
 			}
 		}
 		
-		dx = new int[] {-1, 0, 1, 0};
-		dy = new int[] {0, 1, 0, -1};
-		result = -1;
+		boolean[][][][] visited = new boolean[n][m][n][m];
+		q.add(new Ball(0, x, y, x2, y2));
 		
-		pq = new PriorityQueue<RBO>();
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, 1, 0, -1};
+		int result = -1;
 		
-		bfs(x, y, x_b, y_b, 0, map);
+		while(!q.isEmpty() && result == -1) {
+			
+			Ball b = q.poll();
+//			System.out.println("poll" + b.count);
+			if(b.count == 10) {
+				break;
+			}
+			
+			for(int i=0; i<4; i++) {
+				String in = "";
+				
+				int rx = b.rx;	// 단순히 초기화
+				int ry = b.ry;
+				int bx = b.bx;
+				int by = b.by;
+//				System.out.println("next");
+				
+				while(true) {
+					int nrx = rx + dx[i];
+					int nry = ry + dy[i];
+					int nbx = bx + dx[i];
+					int nby = by + dy[i];
+					int move = 0;
+					
+					if(in.length() == 0) {
+						if(map[nrx][nry].equals("O")) {
+							in += "R";
+							rx = -1;
+							ry = -1;
+							move++;
+						}else {
+							if(!map[nrx][nry].equals("#")) {
+								if(!(nrx == bx && nry == by)) {
+									rx = nrx;
+									ry = nry;
+									move++;
+								}
+							}
+						}
+					}
+					
+					if(map[nbx][nby].equals("O")) {
+						in += "B";
+						break;
+					}else {
+						if(!map[nbx][nby].equals("#")) {
+							if(!(nbx == rx && nby == ry)) {
+								bx = nbx;
+								by = nby;
+								move++;
+							}
+						}
+					}
+					
+					if(move == 0) {
+						break;
+					}
+				}
+				
+//				System.out.println(in);
+				if(in.length() != 0) {
+					if(in.equals("R")) {
+						result = b.count+1;
+						break;
+					}else {
+						continue;
+					}
+				}else {
+					if(!visited[rx][ry][bx][by]) {
+						visited[rx][ry][bx][by]= true;
+						q.add(new Ball(b.count + 1, rx, ry, bx, by));
+					}
+				}
+			}
+		}
 		
 		sb.append(result);
 		System.out.println(sb);
 		br.close();
 	}
-	
-	static void bfs(int x, int y, int x_b, int y_b, int count, String[][] map) {
-		
-		System.out.println(x + " " + y + " " + x_b + " " + y_b + " " + count);
-		for(int i=1; i<map.length; i++) {
-			for(int j=1; j<map[0].length; j++) {
-				System.out.print(map[i][j]);
-			}
-			System.out.println();
-		}
-		
-		if(count == 10) {
-			return;
-		}
-		
-		move(x, y, x_b, y_b, count+1, map, 0);
-		move(x, y, x_b, y_b, count+1, map, 1);
-		move(x, y, x_b, y_b, count+1, map, 2);
-		move(x, y, x_b, y_b, count+1, map, 3);
-		
-	}
-	
-	static void move(int x, int y, int x_b, int y_b, int count, String[][] map, int direction) {
-		
-		String[][] map_clone = new String[map.length][map[0].length];
-		for(int j=1; j<map.length; j++) {
-			for(int k=1; k<map.length; k++) {
-				map_clone[j][k] = map[j][k];
-			}
-		}
-		
-		while(true) {
-			
-			int nx = x + dx[direction];
-			int ny = y + dy[direction];
-			
-			int nx_b = x_b + dx[direction];
-			int ny_b = y_b + dy[direction];
-			
-			if(map_clone[nx_b][ny_b].equals("O")) {
-				map_clone[x_b][y_b] = ".";
-				result = -2;
-				break;
-			}
-			
-			if(map_clone[nx_b][ny_b].equals(".")) {
-				map_clone[nx_b][ny_b] = "B";
-				map_clone[x_b][y_b] = ".";
-				x_b = nx_b;
-				y_b = ny_b;
-			}
-			
-			if(map_clone[nx][ny].equals("O")) {
-				map_clone[x][y] = ".";
-				result = Math.min(result, count);
-			}
-			
-			if(map_clone[nx][ny].equals(".")) {
-				map_clone[nx][ny] = "R";
-				map_clone[x][y] = ".";
-				x = nx;
-				y = ny;
-			}
-			
-			if((x != nx || y != ny) && (x_b != nx_b || y_b != ny_b)) {
-				break;
-			}
-		}
-		
-		if(result == -2) {
-			result = -1;
-		}else {
-			bfs(x, y, x_b, y_b, count, map_clone);
-		}
-		
-	}
-
 }
 
-class RBO implements Comparable<RBO>{
+class Ball{
 	
-	int x;
-	int y;
-	int x_b;
-	int y_b;
 	int count;
-	String[][] map;
+	int rx;
+	int ry;
+	int bx;
+	int by;
 	
-	public RBO(int x, int y, int x_b, int y_b, int count, String[][] map) {
-		this.x = x;
-		this.y = y;
-		this.x_b = x_b;
-		this.y_b = y_b;
+	public Ball(int count, int rx, int ry, int bx, int by) {
 		this.count = count;
-		this.map = new String[map.length][map[0].length];
-		
-		for(int i=1; i<map.length; i++) {
-			for(int j=1; j<map[0].length; j++) {
-				this.map[i][j] = map[i][j];
-			}
-		}
-		
-		
-	}
-
-	@Override
-	public int compareTo(RBO o) {
-		
-		return this.count - o.count ;
+		this.rx = rx;
+		this.ry = ry;
+		this.bx = bx;
+		this.by = by;
 	}
 }
+
--- src/samsung/Q19237.java
+++ src/samsung/Q19237.java
@@ -19,16 +19,16 @@ public class Q19237 {
 		
 		int[][] map = new int[n][n];
 		int[][] smell = new int[n][n];
-		int[][] shark = new int[m+1][4];
+		int[][] shark = new int[m+1][4];	// [상어번호][x, y, 방향, 생존] 0은 비울거다.
 		
 		for(int i=0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				smell[i][j] = k;
 				if(map[i][j] != 0) {
 					shark[map[i][j]][0] = i;
 					shark[map[i][j]][1] = j;
+					smell[i][j] = k;
 				}
 			}
 		}
@@ -38,40 +38,47 @@ public class Q19237 {
 			shark[i][2] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[][][] direction = new int[m+1][5][5];
+		int[][][] direction = new int[m+1][5][5];	// [상어번호][현재방향][다음방향] 0은 비울거다.
 		
 		for(int i=1; i<=m; i++) {
 			for(int j=1; j<=4; j++) {
 				st = new StringTokenizer(br.readLine());
 				for(int l=1; l<=4; l++) {
-					direction[i][j][k] = Integer.parseInt(st.nextToken());
+					direction[i][j][l] = Integer.parseInt(st.nextToken());
 				}
 			}
 		}
 		
-		int[] dx = {0, -1, 1, 0, 0};
-		int[] dy = {0, 0, 0, -1, 1};
-		int count = m;
+		int[] dx = {0, -1, 1, 0, 0};	// 0은 비울거다.
+		int[] dy = {0, 0, 0, -1, 1};	// 0은 비울거다.
+		int count = m;	// 처음 상어 수
 		int time = 0;
 		
-		while(count != 1 && time <= 1001) {
+		while(count != 1 && time <= 1001) {	// 상어가 하나 남거나, 1000초 초과.
 			
-			if(time < 10) {
-				System.out.println(time);
-				for(int a=0; a<n; a++) {
-					for(int b=0; b<n; b++) {
-						System.out.print(map[a][b] + " ");
-					}
-					System.out.println();
-				}
-			}
+//			if(time < 20) {
+//				System.out.println(time);
+//				for(int a=0; a<n; a++) {
+//					for(int b=0; b<n; b++) {
+//						System.out.print(map[a][b] + " ");
+//					}
+//					System.out.println();
+//				}
+//				
+//				for(int a=0; a<n; a++) {
+//					for(int b=0; b<n; b++) {
+//						System.out.print(smell[a][b] + " ");
+//					}
+//					System.out.println();
+//				}
+//			}
 			
 			for(int i=1; i<=m; i++) {
-				if(shark[i][3] == 1) {
+				if(shark[i][3] == 1) {	// 죽은 상어다.
 					continue;
 				}
 				
-				int confirm = 0;
+				int confirm = 0;	// 빈 좌표 찾는 단계.
 				
 				for(int j=1; j<=8; j++) {
 					
@@ -79,16 +86,16 @@ public class Q19237 {
 					
 					if(j_2 > 4) {	// 냄새 없는 곳이 없음.
 						j_2 -= 4;
-						confirm = 1;
+						confirm = 1;	// 자기 냄새 찾는 단계.
 					}
-					//뭔가 하긴하니
+					
 					int dir = direction[i][shark[i][2]][j_2];
 					int nx = shark[i][0] + dx[dir];
 					int ny = shark[i][1] + dy[dir];
 					
-					if(time < 10) {
-						System.out.println("뭔가 하긴 하니 " + i + " " + nx + " " + ny + " " + confirm);
-					}
+//					if(time < 10) {
+//						System.out.println("뭔가 하긴 하니 " + i + " " + nx + " " + ny + " " + confirm);
+//					}
 					
 					if(nx >= 0 && ny >= 0 && nx < n && ny < n) {
 						if(confirm == 0) {
@@ -98,24 +105,27 @@ public class Q19237 {
 									shark[i][1] = ny;
 									shark[i][2] = dir;
 									map[nx][ny] = i;
-									if(time < 10) {
-										System.out.println("move_1 " + i);
-									}
-								}else {
-									if(nx == shark[map[nx][ny]][0] && ny == shark[map[nx][ny]][1]) {
+//									if(time < 10) {
+//										System.out.println("move_1 " + i);
+//									}
+								}else {	
+									// 더 센상어가 먼저 점거, 냄새는 마지막에 뿌리니까 이동은 가능하게 해놨음.
+									if(nx == shark[map[nx][ny]][0] && ny == shark[map[nx][ny]][1] 
+											&& shark[map[nx][ny]][3] == 0) {	// 마지막 조건은 죽은 상어는 무시.
 										shark[i][3] = 1;	// 죽음.
 										count -= 1;
-										if(time < 10) {
-											System.out.println("death " + i);
-										}
+//										if(time < 10) {
+//											System.out.println("death " + i);
+//										}
+										
 									}else {	// 옛날에 다녀간 곳이라 상관없음.
 										shark[i][0] = nx;
 										shark[i][1] = ny;
 										shark[i][2] = dir;
 										map[nx][ny] = i;
-										if(time < 10) {
-											System.out.println("move_2 " + i);
-										}
+//										if(time < 10) {
+//											System.out.println("move_2 " + i);
+//										}
 									}
 								}
 								
@@ -128,9 +138,9 @@ public class Q19237 {
 								shark[i][0] = nx;
 								shark[i][1] = ny;
 								shark[i][2] = dir;
-								if(time < 10) {
-									System.out.println("move_3 " + i);
-								}
+//								if(time < 10) {
+//									System.out.println("move_3 " + i);
+//								}
 								break;
 							}else {
 								continue;	// 자기 냄새가 아님.
@@ -144,8 +154,10 @@ public class Q19237 {
 			}
 			
 			time++;
-			for(int i=1; i<=m; i++) {
-				smell[shark[i][0]][shark[i][1]] = time + k;
+			for(int i=1; i<=m; i++) {	// 마지막에 냄새 뿌림.
+				if(shark[i][3] == 0) {
+					smell[shark[i][0]][shark[i][1]] = time + k;
+				}
 			}
 		}
 		
