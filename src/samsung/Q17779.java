6ce34ccb78d7ea2470429244ea530efe430ab0e7
bestfarmer94 <sjasjaruddus@naver.com>
2022-11-29 17:25:28 +0900

    Samsung24+

--- src/samsung/Q17779.java
+++ src/samsung/Q17779.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Q17779 {
+public class Q17779 {	// 문제에 오류가 있어보인다. 그대로 식을 써봤는데, 5번 선거구가 길어질 경우 범위 설정이 애매하여 선거구가 삐져나온다.
+						// 그래서 그냥 직접 새로운 식으로 구현했다.
 
 	static int n;
 	static int answer;
@@ -33,7 +34,7 @@ public class Q17779 {
 		}
 		
 		answer = Integer.MAX_VALUE;
-		sum = new int[5];
+		sum = new int[6];
 		
 		for(int x=1; x<=n-2; x++) {
 			for(int y=2; y<=n-1; y++) {
@@ -60,44 +61,80 @@ public class Q17779 {
 			Arrays.fill(area[i], 0);
 		}
 		
-		for(int i=0; i<=d1; i++) {
-			area[x+i][y-i] = 5;
-			for(int j=0; j<=d2; j++) {
-				area[x+j][y+j] = 5;
-				area[x+i+j][y-i+j] = 5;
-			}
-		}
-		
-		for(int i=x; i<=x+d1+d2; i++) {
-			for(int j=y-d1; j<=y+d2; j++) {
-				if(i)
-				System.out.print(area[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
 		for(int i=1; i<n+1; i++) {
 			for(int j=1; j<n+1; j++) {
-				if(area[i][j] == 0) {
-					if(i < x+d1 && j <= y) {
+				int start = y-i+x;
+				int end = y+i-x;
+				
+				if(i > x+d1) {
+					start = y-d1 - ((y-i+x)-(y-d1));
+				}
+				if(i > x+d2) {
+					end = y+d2 - ((y+i-x)-(y+d2));
+				}
+				
+				if(j >= start && j <= end) {
+					area[i][j] = 5;
+					sum[5] += map[i][j];
+				}
+				
+				if(i >= 1 && i < x) {
+					if(j <= y) {
 						area[i][j] = 1;
-					}else if(i <= x+d2 && j > y) {
-						area[i][j] = 2;
-					}else if(x+d1 <= i && j < y-d1+d2) {
-						area[i][j] = 3;
-					}else if(x+d2 < i && y-d1+d2 <= j) {
-						area[i][j] = 4;
+						sum[1] += map[i][j];
 					}else {
-						area[i][j] = 5;
+						area[i][j] = 2;
+						sum[2] += map[i][j];
+					}
+					
+				}else if(i >= x && i <= x+d1+d2) {
+					if(i < x+d1 && j < start) {
+						area[i][j] = 1;
+						sum[1] += map[i][j];
+					}
+					
+					if(i <= x+d2 && j > end) {
+						area[i][j] = 2;
+						sum[2] += map[i][j];
+					}
+					
+					if(i >= x+d1 && j < start) {
+						area[i][j] = 3;
+						sum[3] += map[i][j];
+					}
+					
+					if(i > x+d2 && j > end) {
+						area[i][j] = 4;
+						sum[4] += map[i][j];
+					}
+				}else {
+					if(j < y-d1+d2) {
+						area[i][j] = 3;
+						sum[3] += map[i][j];
+					}else {
+						area[i][j] = 4;
+						sum[4] += map[i][j];
 					}
 				}
 			}
 		}
 		
+//		for(int i=1; i<n+1; i++) {
+//			for(int j=1; j<n+1; j++) {
+//				sum[area[i][j]] += map[i][j];
+//				if(area[i][j] == 5) {
+//					System.out.print("■ ");
+//				}else {
+//					System.out.print(area[i][j] + " ");
+//				}
+//			}
+//			System.out.println();
+//		}
+		
 		int max = 0;
 		int min = Integer.MAX_VALUE;
 		
-		for(int i=0; i<5; i++) {
+		for(int i=1; i<6; i++) {
 			if(sum[i] < min) {
 				min = sum[i];
 			}
@@ -107,7 +144,6 @@ public class Q17779 {
 			}
 		}
 		
-		System.out.println(max - min);
 		answer = Math.min(answer, max-min);
 	}
 }
